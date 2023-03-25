16c7a8a535c485ecc48a8f2023ec5feb4ad81959
Rajul thakur <85017445+RajultThakur@users.noreply.github.com>
2023-03-25 15:52:37 +0530

    update mongo db credential

--- Server/Key.js
+++ Server/Key.js
@@ -1,4 +1,4 @@
 module.exports = {
-    SECRET_KEY : "RAROHSOUJULITRASIDBDHARTH",
-    MONGO_URL : "mongodb://localhost:27017/amazondb"
+    SECRET_KEY : "mySecretKey",
+    MONGO_URL : "your mongodb url"
 }
